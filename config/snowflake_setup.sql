4ec6f1390cf2a78bdeb7fac56cc4e9b27e703a57
Tomas Santos Yciano <tomassantos484@gmail.com>
2026-03-05 03:04:37 -0500

    Modifying setup script
    Signed-off-by: Tomas Santos Yciano <tomassantos484@gmail.com>

--- config/snowflake_setup.sql
+++ config/snowflake_setup.sql
@@ -1,11 +1,11 @@
--- Snowflake Setup
 USE ROLE ACCOUNTADMIN;
-
 CREATE DATABASE IF NOT EXISTS EYADC;
 CREATE SCHEMA IF NOT EXISTS EYADC.INTEGRATIONS;
+CREATE SCHEMA IF NOT EXISTS EYADC.REPO;
 
 USE DATABASE EYADC;
 USE SCHEMA INTEGRATIONS;
+
 CREATE NETWORK RULE IF NOT EXISTS PYPI_NETWORK_RULE
   MODE = EGRESS
   TYPE = HOST_PORT
@@ -15,43 +15,28 @@ CREATE NETWORK RULE IF NOT EXISTS PLANETARY_COMPUTER_NETWORK_RULE
   MODE = EGRESS
   TYPE = HOST_PORT
   VALUE_LIST = (
-    -- Primary API endpoints
     'planetarycomputer.microsoft.com',
     'api.planetarycomputer.microsoft.com',
     'planetarycomputer.microsoft.com:443',
-    
-    -- STAC specification endpoints
     'api.stacspec.org',
     'stacspec.org',
-    
-    -- Azure Blob Storage (needed for data access)
     'planetarycomputer.blob.core.windows.net',
     'cpdataeuwest.blob.core.windows.net',
     'ai4edataeuwest.blob.core.windows.net',
     'naipeuwest.blob.core.windows.net',
-    
-    -- Azure Data Lake Storage (for Zarr access)
     'planetarycomputer.dfs.core.windows.net',
     'cpdataeuwest.dfs.core.windows.net',
-    
-    -- SAS token and authentication endpoints
     '*.blob.core.windows.net',
     '*.dfs.core.windows.net',
-    
-    -- Microsoft authentication (if needed)
     'login.microsoftonline.com',
     'management.azure.com'
   );
-
 CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION DATA_CHALLENGE_EXTERNAL_ACCESS
   ALLOWED_NETWORK_RULES = (
-    EYADC.PUBLIC.PYPI_NETWORK_RULE,
-    EYADC.PUBLIC.PLANETARY_COMPUTER_NETWORK_RULE
+    EYADC.INTEGRATIONS.PYPI_NETWORK_RULE,
+    EYADC.INTEGRATIONS.PLANETARY_COMPUTER_NETWORK_RULE
   )
   ENABLED = TRUE;
 
-
--- Verify integration creation
+SHOW NETWORK RULES IN SCHEMA EYADC.INTEGRATIONS;
 DESCRIBE INTEGRATION DATA_CHALLENGE_EXTERNAL_ACCESS;
-
-ALTER ACCOUNT SET USE_WORKSPACES_FOR_SQL = 'always';
